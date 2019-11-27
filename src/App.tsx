c93c5c703685348e26884485511f03524b89e00a
Kirill Dolgikh <kirill.dolgikh@saritasa.com>
2019-11-27 16:11:53 +0700

    Add loader and configure tsconf.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Loader from 'react-loader-spinner';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      
+      <Loader />
     </div>
   );
 }
